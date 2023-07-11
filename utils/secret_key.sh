8ef6029c53447acbcc827c3828c58d95136762e1
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-07-11 16:39:24 -0700

    Add licensing headers everywhere
    Signed-off-by: Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>

--- utils/secret_key.sh
+++ utils/secret_key.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+# SPDX-License-Identifier: CC0-1.0
+#
+# utils/secret_key.sh
+#
+# This file is a component of ShadyURL by Elizabeth Myers.
+#
+# To the extent possible under law, the person who associated CC0 with
+# ShadyURL has waived all copyright and related or neighboring rights
+# to ShadyURL.
+#
+# You should have received a copy of the CC0 legalcode along with this
+# work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 echo "$(LC_ALL=C tr -dc '[:alnum:]+_<>,.-!@%^&*()[]{}\|:;?/`' </dev/urandom | head -c 64)"
