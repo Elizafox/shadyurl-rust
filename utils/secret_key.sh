e7178c7ea7f013f051bc74fbf55dfcc712f8ac37
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-07-26 16:26:37 -0700

    Try to find a base64 utility on the system
    Signed-off-by: Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>

--- utils/secret_key.sh
+++ utils/secret_key.sh
@@ -12,5 +12,32 @@
 # You should have received a copy of the CC0 legalcode along with this
 # work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+# Attempt to find a base64 utility
+for u in "base64" "b64encode" "uuencode" "openssl"; do
+	if [ -n "$(which "$u")" ]; then
+		UTIL="$u"
+		break
+	fi
+done
+
+case "$UTIL" in
+base64)
+	UTIL_FLAGS=""
+	;;
+b64encode)
+	UTIL_FLAGS="-r -"
+	;;
+uuencode)
+	UTIL_FLAGS="-m -r -"
+	;;
+openssl)
+	UTIL_FLAGS="base64"
+	;;
+*)
+	echo "No base64 utility found. Please install base64, b64encode, uuencode, or openssl." >&2
+	exit 1
+	;;
+esac
+
 # status=none isn't POSIX, probably doesn't matter but let's keep it portable
-dd if=/dev/random bs=66 count=1 2>/dev/null | base64
+echo "$(dd if=/dev/random bs=66 count=1 2>/dev/null | $UTIL $UTIL_FLAGS | tr -d '\n ')"
